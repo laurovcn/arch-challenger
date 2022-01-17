252d811c8f6de23bd13a9196b0a549b6aba9d56b
lvneto <lauro.neto.1995@gmail.com>
2022-01-17 09:29:11 -0300

    fix query to pure sql

--- src/controller/notes/notes.controller.ts
+++ src/controller/notes/notes.controller.ts
@@ -7,28 +7,23 @@ const prisma = new PrismaClient()
 
 export const findOne = async (request: Request, response: Response) => {
 
-  try { 
+  try {     
 
-    const id: string = request.params.id
+    const id  = request.params.id
+   
+    const notes: any = await prisma.$queryRaw`SELECT * FROM "public"."Notes" WHERE "studentsId" = ${Number(id)}`
 
-    const notes = await prisma.notes.findUnique(
-      {
-        where: {
-          studentsId: Number(id) 
-        }        
-      })       
-    
-      if (notes && notes !== null) {         
-        const media: number = ((notes.n1 + notes.n2 + notes.n3 + notes.n4) / 4)        
-        if (media < 4){ 
-          return response.json({Message: `Aluno reprovado! Média: ${media}`})
-        }
-        if (media <= 4 || media < 6 ) {
-          return response.json({Message: `Aluno em recuperação! Média: ${media}`})
-        } 
-          return response.json({Message: `Aluno aprovado! Média: ${media}`})
+    if (notes && notes.length !== 0) {         
+      const media: number = ((notes.n1 + notes.n2 + notes.n3 + notes.n4) / 4)        
+      if (media < 4){ 
+        return response.json({Message: `Aluno reprovado! Média: ${media}`})
       }
-        return response.json({Message: 'Aluno não cadastrado ou sem notas!'})
+      if (media <= 4 || media < 6 ) {
+        return response.json({Message: `Aluno em recuperação! Média: ${media}`})
+      } 
+        return response.json({Message: `Aluno aprovado! Média: ${media}`})
+    }
+      return response.json({Message: 'Aluno não cadastrado ou sem notas!'})
 
   } catch (error) {
 
